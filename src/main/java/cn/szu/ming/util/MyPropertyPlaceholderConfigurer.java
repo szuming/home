39c8c91a700c78317cb0325d6e4faa670e60695b
ming <2015150308@email.szu.edu.cn>
2018-05-06 09:37:45 +0800

    实现数据库关键信息的隐藏

---
+++ src/main/java/cn/szu/ming/util/MyPropertyPlaceholderConfigurer.java
@@ -0,0 +1,28 @@
+package cn.szu.ming.util;
+
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MyPropertyPlaceholderConfigurer extends PropertyPlaceholderConfigurer {
+    Map<String,String> map=new HashMap<>();
+
+    public MyPropertyPlaceholderConfigurer(){
+        map.put("username","ming");
+        map.put("password","qq147741");
+        map.put("url","jdbc:mysql://127.0.0.1:3306/home");
+    }
+    @Override
+    protected String convertProperty(String propertyName, String propertyValue) {
+        String temp=map.get(propertyName);
+        if(temp!=null){
+            System.out.println(temp);
+            return temp;
+        }
+        return super.convertProperty(propertyValue,propertyValue);
+    }
+
+
+}
+
