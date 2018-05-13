0c054e15152e1aa04c9272fe31a5e81e1a657100
ming <2015150308@email.szu.edu.cn>
2018-05-13 20:59:00 +0800

    对密码进行password()方法加密

--- src/main/java/cn/szu/ming/controller/htownController.java
+++ src/main/java/cn/szu/ming/controller/htownController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-import redis.clients.jedis.Jedis;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
